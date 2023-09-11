1dc3fd8a28186b76129adfdf832ffbd1b3f6e90f
Elad Tsfany <eladtsfany@gmail.com>
2023-09-11 18:55:26 +0300

    Upadte the css so the grid will be at a fixed pixel-size

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 
-// a function that generates an NxN grid and returns it.
+// a function that creates an NxN grid and returns it.
 function GenerateGrid(n) {
     const grid = document.createElement('div');
     for (let i = 0; i < n; i++) {
         const row = document.createElement('div');
         row.className = 'row';
         for (let j = 0; j < n; j++) {
-            const column = document.createElement('div');
-            column.className = 'column';
-            row.appendChild(column);
+            const cell = document.createElement('div');
+            cell.className = 'cell';
+            row.appendChild(cell);
         }
         grid.appendChild(row);
     }
     grid.className = 'grid';
+    grid.addEventListener('mouseover', e => {
+        if (e.target.className === 'cell')
+            e.target.style.backgroundColor = 'green';
+    })
     return grid;
 }
-const grid16 = GenerateGrid(16);
-document.body.appendChild(grid16);
 
-grid16.addEventListener('mouseover', e => {
-    if (e.target.className === 'column')
-        e.target.style.backgroundColor = 'green';
-})
+let startGrid = GenerateGrid(16);
+document.body.appendChild(startGrid);
+
+const btn = document.querySelector('.btn').addEventListener('click', newGrid);
+
+function newGrid() {
+    const input = parseInt(prompt("Number of squares per side 0-100"));
+    if (typeof input !== 'number' || isNaN(input)) {
+        alert("Error: Invalid input; has to be a number.");
+        return;
+    }
+    else if (input > 100 || input < 0) {
+        alert("Error: Grid size has to be between 0 and 100");
+        return;
+    }
+    //remove existing grid
+    document.querySelector('.grid').remove();
+    //create new grid and append
+    const userGrid = GenerateGrid(input);
+    document.body.appendChild(userGrid);
+}
